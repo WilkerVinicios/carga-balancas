41db8683fd3ae6a7c9cdb26606402dd9ecb0ad87
Wilker Vinicios <willkervinicios@gmail.com>
2021-01-19 10:09:03 -0300

    Criando Models;

---
+++ src/main/java/model/Produto.java
@@ -0,0 +1,63 @@
+package model;
+
+public class Produto {
+    private String departamento;
+    private String unidadeMedida;
+    private String ean;
+    private String precoVenda;
+    private String validade;
+    private String descricao;
+
+    public Produto() {
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getUnidadeMedida() {
+        return unidadeMedida;
+    }
+
+    public void setUnidadeMedida(String unidadeMedida) {
+        this.unidadeMedida = unidadeMedida;
+    }
+
+    public String getEan() {
+        return ean;
+    }
+
+    public void setEan(String ean) {
+        this.ean = ean;
+    }
+
+    public String getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(String precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public String getValidade() {
+        return validade;
+    }
+
+    public void setValidade(String validade) {
+        this.validade = validade;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
+
---
+++ src/main/java/model/TabelaNutricional.java
@@ -0,0 +1,154 @@
+package model;
+
+import java.math.BigDecimal;
+
+public class TabelaNutricional {
+
+    private int codNutri;
+    private BigDecimal quantidadePorPorcao;
+    private BigDecimal unidadePorcao;
+    private BigDecimal parteInteiraMedidaCaseira;
+    private BigDecimal parteDecimalMedidaCaseira;
+    private BigDecimal medidaCaseiraUtilizada;
+    private BigDecimal valorCalorico;
+    private BigDecimal valorCarboidratos;
+    private BigDecimal valorProteinas;
+    private BigDecimal valorGorduraTotal;
+    private BigDecimal valorGorduraSaturada;
+    private BigDecimal valorGorduraTrans;
+    private BigDecimal valorFibra;
+    private BigDecimal valorSodio;
+
+    public TabelaNutricional() {
+    }
+
+    public TabelaNutricional(int codNutri, BigDecimal quantidadePorPorcao, BigDecimal unidadePorcao, BigDecimal parteInteiraMedidaCaseira, BigDecimal parteDecimalMedidaCaseira, BigDecimal medidaCaseiraUtilizada, BigDecimal valorCalorico, BigDecimal valorCarboidratos, BigDecimal valorProteinas, BigDecimal valorGorduraTotal, BigDecimal valorGorduraSaturada, BigDecimal valorGorduraTrans, BigDecimal valorFibra, BigDecimal valorSodio) {
+        this.codNutri = codNutri;
+        this.quantidadePorPorcao = quantidadePorPorcao;
+        this.unidadePorcao = unidadePorcao;
+        this.parteInteiraMedidaCaseira = parteInteiraMedidaCaseira;
+        this.parteDecimalMedidaCaseira = parteDecimalMedidaCaseira;
+        this.medidaCaseiraUtilizada = medidaCaseiraUtilizada;
+        this.valorCalorico = valorCalorico;
+        this.valorCarboidratos = valorCarboidratos;
+        this.valorProteinas = valorProteinas;
+        this.valorGorduraTotal = valorGorduraTotal;
+        this.valorGorduraSaturada = valorGorduraSaturada;
+        this.valorGorduraTrans = valorGorduraTrans;
+        this.valorFibra = valorFibra;
+        this.valorSodio = valorSodio;
+    }
+
+    public int getCodNutri() {
+        return codNutri;
+    }
+
+    public void setCodNutri(int codNutri) {
+        this.codNutri = codNutri;
+    }
+
+    public BigDecimal getQuantidadePorPorcao() {
+        return quantidadePorPorcao;
+    }
+
+    public void setQuantidadePorPorcao(BigDecimal quantidadePorPorcao) {
+        this.quantidadePorPorcao = quantidadePorPorcao;
+    }
+
+    public BigDecimal getUnidadePorcao() {
+        return unidadePorcao;
+    }
+
+    public void setUnidadePorcao(BigDecimal unidadePorcao) {
+        this.unidadePorcao = unidadePorcao;
+    }
+
+    public BigDecimal getParteInteiraMedidaCaseira() {
+        return parteInteiraMedidaCaseira;
+    }
+
+    public void setParteInteiraMedidaCaseira(BigDecimal parteInteiraMedidaCaseira) {
+        this.parteInteiraMedidaCaseira = parteInteiraMedidaCaseira;
+    }
+
+    public BigDecimal getParteDecimalMedidaCaseira() {
+        return parteDecimalMedidaCaseira;
+    }
+
+    public void setParteDecimalMedidaCaseira(BigDecimal parteDecimalMedidaCaseira) {
+        this.parteDecimalMedidaCaseira = parteDecimalMedidaCaseira;
+    }
+
+    public BigDecimal getMedidaCaseiraUtilizada() {
+        return medidaCaseiraUtilizada;
+    }
+
+    public void setMedidaCaseiraUtilizada(BigDecimal medidaCaseiraUtilizada) {
+        this.medidaCaseiraUtilizada = medidaCaseiraUtilizada;
+    }
+
+    public BigDecimal getValorCalorico() {
+        return valorCalorico;
+    }
+
+    public void setValorCalorico(BigDecimal valorCalorico) {
+        this.valorCalorico = valorCalorico;
+    }
+
+    public BigDecimal getValorCarboidratos() {
+        return valorCarboidratos;
+    }
+
+    public void setValorCarboidratos(BigDecimal valorCarboidratos) {
+        this.valorCarboidratos = valorCarboidratos;
+    }
+
+    public BigDecimal getValorProteinas() {
+        return valorProteinas;
+    }
+
+    public void setValorProteinas(BigDecimal valorProteinas) {
+        this.valorProteinas = valorProteinas;
+    }
+
+    public BigDecimal getValorGorduraTotal() {
+        return valorGorduraTotal;
+    }
+
+    public void setValorGorduraTotal(BigDecimal valorGorduraTotal) {
+        this.valorGorduraTotal = valorGorduraTotal;
+    }
+
+    public BigDecimal getValorGorduraSaturada() {
+        return valorGorduraSaturada;
+    }
+
+    public void setValorGorduraSaturada(BigDecimal valorGorduraSaturada) {
+        this.valorGorduraSaturada = valorGorduraSaturada;
+    }
+
+    public BigDecimal getValorGorduraTrans() {
+        return valorGorduraTrans;
+    }
+
+    public void setValorGorduraTrans(BigDecimal valorGorduraTrans) {
+        this.valorGorduraTrans = valorGorduraTrans;
+    }
+
+    public BigDecimal getValorFibra() {
+        return valorFibra;
+    }
+
+    public void setValorFibra(BigDecimal valorFibra) {
+        this.valorFibra = valorFibra;
+    }
+
+    public BigDecimal getValorSodio() {
+        return valorSodio;
+    }
+
+    public void setValorSodio(BigDecimal valorSodio) {
+        this.valorSodio = valorSodio;
+    }
+
+}
